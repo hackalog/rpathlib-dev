0fa20494785bc656d316874d8d5f01dc5f239fa5
Kjell Wooding <kjell@wooding.org>
2021-09-10 11:36:10 -0600

    initial import

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='2.0.0',
+    description='''Strawman for an 'rpathlib' implementation''',
+    author='Kjell Wooding',
+    license='MIT',
+)
